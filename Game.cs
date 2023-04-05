c92ba8bfbadb77bfead40e7443334dc91b11d897
Jose Cabrera <murdoc.jose.6@gmail.com>
2023-04-05 12:29:26 -0300

    feat: added blend colors in the triangle area

--- Game.cs
+++ Game.cs
@@ -12,9 +12,21 @@ namespace Physic_Engine
         private int shaderProgramHandle;
         private int vertexArrayHandle;
 
-        public Game() : base(GameWindowSettings.Default, NativeWindowSettings.Default)
+        public Game(int width = 1280, int height = 768, string title = "Physic Engine") : base(
+            GameWindowSettings.Default,
+            new NativeWindowSettings()
+            {
+                Title = title,
+                Size = new Vector2i(width, height),
+                WindowBorder = WindowBorder.Fixed,
+                StartVisible = false,
+                StartFocused = true,
+                API = ContextAPI.OpenGL,
+                Profile = ContextProfile.Core,
+                APIVersion = new Version(3, 3),
+            })
         {
-            this.CenterWindow(new Vector2i(1280, 720));
+            this.CenterWindow();
         }
 
         protected override void OnResize(ResizeEventArgs e)
@@ -25,12 +37,14 @@ namespace Physic_Engine
 
         protected override void OnLoad()
         {
+
+            this.IsVisible = true;
             GL.ClearColor(new Color4(0.3f, 0.4f, 0.5f, 1.0f));
 
             float[] vertices = new float[]{
-                0f, 0.5f, 0f, // vertex 0
-                0.5f, -0.5f, 0f, // vertex 1
-                -0.5f, -0.5f, 0f // vertex 2
+                0f, 0.5f, 0f, 1f, 0f, 0f, 1f, // vertex 0
+                0.5f, -0.5f, 0f, 0f, 1f, 0f, 1f, // vertex 1
+                -0.5f, -0.5f, 0f, 0f, 0f, 1f, 1f, // vertex 2
             };
 
 
@@ -43,8 +57,10 @@ namespace Physic_Engine
             GL.BindVertexArray(this.vertexArrayHandle);
 
             GL.BindBuffer(BufferTarget.ArrayBuffer, this.vertextBufferHandle);
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 7 * sizeof(float), 0);
+            GL.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 7 * sizeof(float), 3 * sizeof(float));
             GL.EnableVertexAttribArray(0);
+            GL.EnableVertexAttribArray(1);
 
             GL.BindVertexArray(0);
 
@@ -52,18 +68,26 @@ namespace Physic_Engine
                 @"
                 #version 330 core
                 layout (location = 0) in vec3 aPosition;
+                layout (location = 1) in vec4 aColor;
+
+                out vec4 vColor;
+
                 void main()
                 {
+                    vColor = aColor;
                     gl_Position = vec4(aPosition.x, aPosition.y, aPosition.z, 1.0);
                 }";
 
             string pixelShaderCode =
                 @"
                 #version 330 core
-                out vec4 FragColor;
+
+                in vec4 vColor;
+
+                out vec4 pixelColor;
                 void main()
                 {
-                    FragColor = vec4(0.8f, 0.8f, 0.1f, 0.1f);
+                    pixelColor = vColor;
                 }";
 
             int vertexShaderHandle = GL.CreateShader(ShaderType.VertexShader);
@@ -92,6 +116,9 @@ namespace Physic_Engine
 
         protected override void OnUnload()
         {
+            GL.BindVertexArray(0);
+            GL.DeleteVertexArray(this.vertexArrayHandle);
+
             GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
             GL.DeleteBuffer(this.vertextBufferHandle);
 
